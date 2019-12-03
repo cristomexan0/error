3c5c07ee5629b56c81af51e29d71fa10d0d5c336
cristomexan0 <42562225+cristomexan0@users.noreply.github.com>
2019-12-03 12:26:12 -0800

    Trabajo en clase

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace error
+{
+    class NegativeIntergetException:Exception
+    {
+        public NegativeIntergetException():base("Es un error")
+        {
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Captura un entero positivo");
+            int valor = 0;
+            try
+            {
+                valor = int.Parse(Console.ReadLine());
+                if (valor < 0 )             
+                {
+                    throw new NegativeIntergetException();
+                }
+                Console.WriteLine("El valor es:{0}", valor);
+                Main();
+            }
+            catch ( StackOverflowException e)                                    //System.OverflowException e
+            {
+                Console.WriteLine("Hubo un error: ");
+                Console.WriteLine(e.Message);
+            }
+            //
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+
+        }
+    }
+}
